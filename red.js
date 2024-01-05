c8f0e79e09946f41774437b25076c73e9cd61d48
phkl1 <155610411+phkl1@users.noreply.github.com>
2024-01-05 17:38:47 +0700

    added red.html

---
+++ red.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 80) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
